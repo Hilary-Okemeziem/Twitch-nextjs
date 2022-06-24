18e4487a314074c2e4c83e910b9426ce4c1013a7
Hilary Okemeziem <hilaryokems@gmail.com>
2022-06-24 22:19:37 +0100

    debugged errors

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div id='hero' className='p-2 md:p-8'>
+    <div id='hero' className='p-2 mt-2 md:p-8'>
         <div className='relative overflow-hidden w-full pt-[56.25%]'>
             <iframe className='absolute top-0 left-0 right-0 bottom-0 w-full h-full' src="https://www.youtube.com/embed/MaXHRlzsDMg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
